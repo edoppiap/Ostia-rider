fc050449aa96d2774675f888807815884907ad14
edoppiap <edoppiap@gmail.com>
2021-10-11 12:53:58 +0200

    Aggiunto Start Menu
    Implementato uno start menu primitivo con un Canvas con la possibilità di cliccare su oggetti fisici a schermo

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Cameras")]
+    public CinemachineVirtualCamera mainCamera;
+    public CinemachineVirtualCamera mainMenuCamera;
+    //public Camera MainCamera;
+    //public Camera MainMenuCamera;
+    public Canvas GameCanvas;
+    public Canvas MainMenuCanvas;
+
+    [Header("Parents")]
     public GameObject restaurantsParent;
     public GameObject clientsParent;
 
@@ -23,7 +33,7 @@ public class GameManager : MonoBehaviour
                 Random.InitState(System.DateTime.Now.Millisecond);
                 int i = Mathf.RoundToInt(Random.Range(0, clients.Count - 1));
                 scriptCliente.SetCliente(clients[i]);
-                clients.RemoveAt(i);
+                //clients.RemoveAt(i);
             }
 
             if (scriptCliente.GetCliente() == null)
@@ -31,6 +41,15 @@ public class GameManager : MonoBehaviour
                 restaurant.SetActive(false);
             }
         }
+    }
+
+    public void StartGame()
+    {
+        mainCamera.Priority = 1;
+        mainMenuCamera.Priority = 0;
+
+        MainMenuCanvas.enabled = false;
+        GameCanvas.enabled = true;
     }
 
     private void AssignTarget(Transform target)
@@ -91,6 +110,12 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
+        mainMenuCamera.Priority = 1;
+        mainCamera.Priority = 0;
+
+        MainMenuCanvas.enabled = true;
+        GameCanvas.enabled = false;
+
         PopulateList(restaurantsParent.GetComponentsInChildren<Transform>(), restaurants);
         PopulateList(clientsParent.GetComponentsInChildren<Transform>(), clients);
 
